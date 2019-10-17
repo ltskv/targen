f77a68508f86cb88b3442d38255a7d98c69fa22d
Pavel Lutskov <pavel.lutskov@gmail.com>
2019-10-16 17:38:23 -0700

    implemented size pre-calculation and sanity check
    and also variable block size for the generator

--- targen.py
+++ targen.py
@@ -1,4 +1,31 @@
 from pathlib import Path
+from math import ceil
+from sys import argv, stdout
+
+
+class TarError(Exception):
+    pass
+
+
+def _normalize_and_prefix(f, base):
+    f = Path(f)
+    base = Path(base)
+
+    if base == f.parent:
+        prefix = ''
+    else:
+        prefix = f.relative_to(base).parent
+
+    return f, base, prefix
+
+
+def _pad(data, s, val=b'\0'):
+    padsize = s - len(data)
+    return data + val*padsize
+
+
+def _as_multiple(a, m):
+    return ceil(a / m) * m
 
 
 def tar_header(f, prefix):
@@ -10,18 +37,18 @@ def tar_header(f, prefix):
         mode = b'000644 \0'
         ftype = b'0'
     else:
-        raise ValueError(
+        raise TarError(
             f'{f} is not a regular file or a directory'
         )
 
     name = f.name.encode('utf-8')
     prefix = str(prefix).encode('utf-8')
     if len(name) > 99:
-        raise ValueError(f'Length of file name of {f.name} exceeds 99')
+        raise TarError(f'Length of file name of {f.name} exceeds 99')
     if len(prefix) > 154:
-        raise ValueError(f'Length of prefix name ({prefix}) exceeds 154')
+        raise TarError(f'Length of prefix name ({prefix}) exceeds 154')
 
-    h = bytearray(name + b'\0'*(100-len(name)))  # file name: 100 bytes
+    h = bytearray(_pad(name, 100))  # file name: 100 bytes
     h += mode # file mode: 8 bytes
     h += b'000777 \0'  # file owner id: 8 bytes
     h += b'000777 \0'  # file group id: 8 bytes
@@ -42,7 +69,7 @@ def tar_header(f, prefix):
     h += b'\0' * 32  # symbolic group name, drop it, 32 bytes
     h += b'\0' * 8  # major device number, drop it, 8 bytes
     h += b'\0' * 8  # minor device number, drop it, 8 bytes
-    h += prefix + b'\0'*(155-len(prefix))  # prefix name, 155 bytes
+    h += _pad(prefix, 155)  # prefix name, 155 bytes
 
     h += b'\0' * 12  # pad to 512
 
@@ -52,15 +79,29 @@ def tar_header(f, prefix):
     return bytes(h)
 
 
-def targen(f, base):
+def check(f, base):
+    f, base, prefix = _normalize_and_prefix(f, base)
+    try:
+        tar_header(f, prefix)
+        if f.is_dir():
+            for e in f.iterdir():
+                check(e, base)
+    except TarError as e:
+        print(e)
+        return False
+    return True
+
+
+def calc_size(f):
     f = Path(f)
-    base = Path(base)
+    if f.is_file():
+        return _as_multiple(f.stat().st_size, 512) + 512
+    elif f.is_dir():
+        return sum(calc_size(e) for e in f.iterdir()) + 512
 
-    if base == f.parent:
-        prefix = ''
-    else:
-        prefix = f.relative_to(base).parent
 
+def targen(f, base, bc=1):
+    f, base, prefix = _normalize_and_prefix(f, base)
     yield tar_header(f, prefix)
 
     if f.is_dir():
@@ -69,9 +110,15 @@ def targen(f, base):
     else:
         with open(f, 'rb') as r:
             while True:
-                data = r.read(512)
-                if len(data) < 512:
-                    padsize = 0 if not len(data) else (512 - (len(data) % 512))
-                    yield data + b'\0' * padsize
+                data = r.read(512*bc)
+                if len(data) < 512*bc:
+                    yield _pad(
+                        data, _as_multiple(len(data), 512)
+                    ) if len(data) else b''
                     break
                 yield data
+
+
+if __name__ == '__main__':
+    for b in targen(argv[1], argv[2]):
+        stdout.buffer.write(b)
